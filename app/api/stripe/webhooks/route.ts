1014a562880db129d0a5ae7e9749407eaa4bd533
Daniel Gamboa <danielgamboa10@gmail.com>
2025-02-14 12:56:18 -0600

    Try to fix vercel issue

--- app/api/stripe/webhooks/route.ts
+++ app/api/stripe/webhooks/route.ts
@@ -7,7 +7,12 @@ const relevantEvents = new Set(["checkout.session.completed", "customer.subscrip
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const sig = (await headers()).get("Stripe-Signature") as string;
+  const sig = (await headers()).get("Stripe-Signature");
+
+  if (!sig) {
+    throw new Error("Missing Stripe-Signature header");
+  }
+
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
