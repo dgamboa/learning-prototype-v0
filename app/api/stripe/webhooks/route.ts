9c097b7f89ccfde53291d7bc4f72f370a8d7e92b
Daniel Gamboa <danielgamboa10@gmail.com>
2025-02-14 12:22:41 -0600

    Fix bug on stripe route

--- app/api/stripe/webhooks/route.ts
+++ app/api/stripe/webhooks/route.ts
@@ -7,7 +7,7 @@ const relevantEvents = new Set(["checkout.session.completed", "customer.subscrip
 
 export async function POST(req: Request) {
   const body = await req.text();
-  const sig = headers().get("Stripe-Signature") as string;
+  const sig = (await headers()).get("Stripe-Signature");
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
