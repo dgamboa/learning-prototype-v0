c0910e3aaa0d24bab97cb6a1c32331ae0a026d83
Daniel Gamboa <danielgamboa10@gmail.com>
2025-02-17 17:19:49 -0600

    Fix bux with chatId params

--- app/search/[chatId]/page.tsx
+++ app/search/[chatId]/page.tsx
@@ -5,20 +5,31 @@ import ChatArea from "../_components/chat-area"
 import ChatAreaSkeleton from "../_components/chat-area-skeleton"
 import { getMessagesByChatIdAction } from "@/actions/db/messages-actions"
 import { getSourcesByChatIdAction } from "@/actions/db/sources-actions"
+import { auth } from "@clerk/nextjs/server"
 
 export default async function ChatPage({
   params
 }: {
   params: { chatId: string }
 }) {
+  const { userId } = await auth()
+  const {chatId} = await params
+
+  if (!userId) {
+    return <div>Please log in to view this page.</div>
+  }
+
   return (
-    <Suspense fallback={<ChatAreaSkeleton />}>
-      <ChatAreaFetcher chatId={params.chatId} />
-    </Suspense>
+    <div className="h-screen flex-1">
+      <Suspense fallback={<ChatAreaSkeleton />}>
+        <ChatAreaFetcher userId={userId} chatId={chatId} />
+      </Suspense>
+    </div>
   )
 }
 
-async function ChatAreaFetcher({ chatId }: { chatId: string }) {
+
+async function ChatAreaFetcher({ chatId, userId }: { chatId: string, userId: string }) {
   const { data: messages } = await getMessagesByChatIdAction(chatId)
   const { data: sources } = await getSourcesByChatIdAction(chatId)
 
@@ -28,6 +39,7 @@ async function ChatAreaFetcher({ chatId }: { chatId: string }) {
         initialMessages={messages || []} 
         initialSources={sources || []}
         chatId={chatId}
+        userId={userId}
       />
     </div>
   )
