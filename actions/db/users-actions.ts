ed6f7534068b2e31f76d2822ac4c244768965d35
Daniel Gamboa <danielgamboa10@gmail.com>
2025-02-17 14:39:37 -0600

    Update user actions for new type format

--- actions/db/users-actions.ts
+++ actions/db/users-actions.ts
@@ -1,59 +1,59 @@
 "use server";
 
 import { createUser, deleteUser, getAllUsers, getUserByUserId, updateUser } from "@/db/queries/users-queries";
-import { InsertUser } from "@/db/schema/users-schema";
+import { InsertUser, SelectUser } from "@/db/schema/users-schema";
 import { ActionState } from "@/types";
 import console from "console";
 import { revalidatePath } from "next/cache";
 
-export async function createUserAction(data: InsertUser): Promise<ActionState> {
+export async function createUserAction(data: InsertUser): Promise<ActionState<InsertUser>> {
   try {
     const newUser = await createUser(data);
     console.log("New user created", newUser);
     revalidatePath("/");
-    return { status: "success", message: "User created successfully", data: newUser };
+    return { isSuccess: true, message: "User created successfully", data: newUser };
   } catch (error) {
-    return { status: "error", message: "Error creating user" };
+    return { isSuccess: false, message: "Error creating user" };
   }
 }
 
-export async function getUserByUserIdAction(userId: string): Promise<ActionState> {
+export async function getUserByUserIdAction(userId: string): Promise<ActionState<SelectUser>> {
   try {
     const user = await getUserByUserId(userId);
     if (!user) {
-      return { status: "error", message: "User not found" };
+      return { isSuccess: false, message: "User not found" };
     }
-    return { status: "success", message: "User retrieved successfully", data: user };
+    return { isSuccess: true, message: "User retrieved successfully", data: user };
   } catch (error) {
-    return { status: "error", message: "Failed to get user" };
+    return { isSuccess: false, message: "Failed to get user" };
   }
 }
 
-export async function getAllUsersAction(): Promise<ActionState> {
+export async function getAllUsersAction(): Promise<ActionState<SelectUser[]>> {
   try {
     const users = await getAllUsers();
-    return { status: "success", message: "Users retrieved successfully", data: users };
+    return { isSuccess: true, message: "Users retrieved successfully", data: users };
   } catch (error) {
-    return { status: "error", message: "Failed to get users" };
+    return { isSuccess: false, message: "Failed to get users" };
   }
 }
 
-export async function updateUserAction(userId: string, data: Partial<InsertUser>): Promise<ActionState> {
+export async function updateUserAction(userId: string, data: Partial<InsertUser>): Promise<ActionState<SelectUser>> {
   try {
     const updatedUser = await updateUser(userId, data);
     revalidatePath("/users");
-    return { status: "success", message: "User updated successfully", data: updatedUser };
+    return { isSuccess: true, message: "User updated successfully", data: updatedUser };
   } catch (error) {
-    return { status: "error", message: "Failed to update user" };
+    return { isSuccess: false, message: "Failed to update user" };
   }
 }
 
-export async function deleteUserAction(userId: string): Promise<ActionState> {
+export async function deleteUserAction(userId: string): Promise<ActionState<SelectUser>> {
   try {
     await deleteUser(userId);
     revalidatePath("/users");
-    return { status: "success", message: "User deleted successfully" };
+    return { isSuccess: true, message: "User deleted successfully" };
   } catch (error) {
-    return { status: "error", message: "Failed to delete user" };
+    return { isSuccess: false, message: "Failed to delete user" };
   }
 }
